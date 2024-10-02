490b0a408e4bd2ec3bbaf4165e94285859d722d0
Eugene Kluev <me@kluev.eu>
2024-10-02 16:55:14 +0300

    feat: mvp w/o timeouts

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+type userHandler struct {
+	queue *map[string]Deque
+}
+
+func (h *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	queueName := r.URL.Path
+	fmt.Println("start: ", method, queueName, r.URL.Query(), h.queue, (*h.queue)[queueName])
+
+	if method == "PUT" {
+		message := r.URL.Query().Get("v")
+		if len(message) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		deque, exists := (*h.queue)[queueName]
+		if !exists {
+			deque = Deque{}
+		}
+		deque.PushFront(message)
+		(*h.queue)[queueName] = deque
+	}
+
+	if method == "GET" {
+		timeout := r.URL.Query().Get("timeout")
+		// TODO: timeouts while GET request
+		deque, _ := (*h.queue)[queueName]
+		result, exist := deque.PopBack()
+		(*h.queue)[queueName] = deque
+
+		if exist {
+			_, err := w.Write([]byte(result))
+			if err != nil {
+				return
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+
+	fmt.Println("end: ", method, queueName, r.URL.Query(), h.queue, (*h.queue)[queueName])
+}
+
+func main() {
+	queue := make(map[string]Deque)
+	mux := http.NewServeMux()
+	mux.Handle("/", &userHandler{&queue})
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		return
+	}
 }
 
 // Deque however better to use https://github.com/gammazero/deque for O(N) while insert
